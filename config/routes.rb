bfdb8d86f5cb74989b9933dccc0395ceabbcf9f5
venumadhav.dondapati <venudondapati2008@gmail.com>
2016-05-13 15:34:09 -0400

    Adds devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MarketPlaceApi::Application.routes.draw do 
 
+  devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 
   scope module: :v1, 

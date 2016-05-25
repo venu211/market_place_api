ed77d81d06df3857294a5b1acb958407801a689d
venumadhav.dondapati <venudondapati2008@gmail.com>
2016-05-25 18:32:25 -0400

    Remove sabisu configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 MarketPlaceApi::Application.routes.draw do 
 
-  mount SabisuRails::Engine => "/sabisu_rails"
-  devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 
   scope module: :v1, 

1383526e79d98df5397d599921b0b19e2dfa7334
venumadhav.dondapati <venudondapati2008@gmail.com>
2016-05-27 01:02:15 -0400

    Generate authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #validates :email, uniqueness: true
+  validates :auth_token, uniqueness: true
+
+  before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+  	begin
+  		self.auth_token = Devise.friendly_token
+  	end while self.class.exists?(auth_token: auth_token)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,8 @@ describe User do
   it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
-
+  it { should respond_to(:auth_token) }
+  it { should validate_uniqueness_of(:auth_token) }  
   it { should be_valid }
 
 describe "when email is not present" do
@@ -18,6 +19,20 @@ describe "when email is not present" do
 	before { @user.email = ''}
 	it { should_not be_valid }
 end
+
+describe "#generate_authentication_token!" do
+  it "generate a unique token" do
+    Devise.stub(:friendly_token).and_return("auniquetoken123")
+    @user.generate_authentication_token!
+    expect(@user.auth_token).to eql "auniquetoken123"
+  end
+
+  it "generates another token when one already has been taken" do
+     existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+     @user.generate_authentication_token!
+     expect(@user.auth_token).not_to eql existing_user.auth_token
+  end
+end  
   #pending "add some examples to (or delete) #{__FILE__}"
 end
 

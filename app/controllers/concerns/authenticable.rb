fee8ab9d66880b3486b45b2740892bddaf07be6c
venumadhav.dondapati <venudondapati2008@gmail.com>
2016-06-11 18:48:20 -0400

    Add user signed method

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -8,4 +8,8 @@ def authenticate_with_token!
 	render json: {errors: "Not authenticated"}, status: unauthorized unless current_user.present?
 end
 
+def user_signed_in?
+	current_user.present?
+end
+
 end

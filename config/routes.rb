a8933aeb91f73b172ddc1576d8c29b9406183342
venumadhav.dondapati <venudondapati2008@gmail.com>
2016-06-13 11:50:47 -0400

    Add show and index action to product model

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ MarketPlaceApi::Application.routes.draw do
   
   resources :users, :only => [:show, :create, :update, :destroy]
   resources :sessions, :only => [:create, :destroy]
+  resources :products, :only => [:show, :index]
+  
   end
 
 
